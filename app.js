dc961d630367f37049ea7eb137788f40540b2041
Lee <dltpdnd1234@gmail.com>
2021-06-07 21:44:04 -0500

    'mongoDB'

--- app.js
+++ app.js
@@ -1,11 +1,49 @@
-const express = require('express')
+const express = require('express')  
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
 const app = express()
-
-app.listen(4001)
-
+const dbURI = 'mongodb+srv://LeeUser:aa123456@cluster0.8kbcr.mongodb.net/node-tutor?retryWrites=true&w=majority'
+mongoose.connect(dbURI, {useNewUrlParser:true,useUnifiedTopology:true})
+  .then((result) => app.listen(4001))
+  .catch((err) => console.log(err))
 // view engine
 app.set('view engine', 'ejs');
+
+// app.use((req,res,next)=>{
+//   console.log('new request made:');
+//   console.log('host: ', req.hostname);
+//   console.log('path: ',req.path);
+//   console.log('method: ', req.method);
+//   next()
+// })
+app.use(morgan('dev'))
+
+app.get('/add-blog',(req,res)=>{
+  const blog = new Blog({
+    title: 'new blog1',
+    snippet: 'about my new blog',
+    body: 'more about my new blog'
+  })
+  blog.save()
+    .then((result)=>{
+      res.send(result)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+})
+
+app.get('/single-blog',(res,req)=>{
+  Blog.find().sort({ createdAt: -1})
+    .then((result)=>{
+      res.send(result)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+})
 
 app.get('/',(req,res)=>{
   // res.send('<p>home page</p>')
